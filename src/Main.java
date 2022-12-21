90f0691dc63e2a44dd093b45065a70c12349721d
line-t-c <line_t_c@hotmail.com>
2022-12-21 12:37:50 +0100

    Commit New Feature

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world! Change 1");
+        System.out.println("Hello world! New Feature");
         System.out.println("Hello people");
         System.out.println("A new change in the original project");
     }

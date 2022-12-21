87e4f31b7abcb77c3c043ae37b67378ff04864e3
line-t-c <line_t_c@hotmail.com>
2022-12-21 12:26:21 +0100

    Commit change again

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 
         System.out.println("Hello world! Change 1");
         System.out.println("Hello people");
+        System.out.println("A new change in the original project");
     }
 }

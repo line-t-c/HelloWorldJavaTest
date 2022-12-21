6ccb1f84d19375ea668db8515e21e8007bcb1cbe
line-t-c <line_t_c@hotmail.com>
2022-12-21 12:28:43 +0100

    Commit clone change again

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world! Change 1");
         System.out.println("Hello people");
         System.out.println("A new change in the original project");
+        System.out.println("now push clone");
     }
 }
